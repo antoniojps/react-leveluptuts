8a7639314cc05f495ce359cfadf27ddf2fa8ad94
António Santos <antoniojps@ua.pt>
2018-11-26 17:52:56 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "settings": {
+        "ecmascript": 6,
+        "jsx": true
+  },
+  "parserOptions": {
+      "ecmaVersion": 2017,
+      "ecmaFeatures": {
+          "experimentalObjectRestSpread": true,
+          "experimentalDecorators": true,
+          "jsx": true
+      },
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react",
+  ],
+  "extends": "airbnb",
+  "rules": {
+    "react/jsx-filename-extension": 0,
+      "function-paren-newline": 0,
+        // allow paren-less arrow functions
+        'arrow-parens': 0,
+        // allow async-await
+        'generator-star-spacing': 0,
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        // Trailing comma
+        'comma-dangle': ['error', 'always-multiline'],
+      'no-unused-vars': ['error', { varsIgnorePattern: 'expect' }],
+      'semi': 0,
+  }
+};
